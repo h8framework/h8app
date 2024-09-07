dfd8466154932109f3501556306f7a733ce453fc
anarkigotic <anarkigotic@gmail.com>
2024-09-07 15:56:04 +0000

    add

--- h8core/check_metaclass.py
+++ h8core/check_metaclass.py
@@ -12,3 +12,22 @@ def check_metaclass(cls, name, bases):
                 raise TypeError(
                     f"Can't create new class {name} with no abstract classmethod {method_name} redefined in the metaclass"
                 )
+
+
+
+
+def check_module_properties(cls, name, bases):
+    if bases and len(bases) > 1:
+        raise TypeError(
+            f"You cannot declare multiple inheritance of a module class. Please check implementation of {name}"
+        )
+
+    if bases:
+        base: type = bases[0]
+        for attr_name, attr_value in vars(base).items():
+            if isinstance(attr_value, property):
+                if not hasattr(cls, attr_name):
+                    raise AttributeError(
+                        f"Property '{attr_name}' must be defined in the class '{name}' derived from '{base.__name__}'"
+                    )
+                
--- h8core/module.py
+++ h8core/module.py
@@ -1,14 +1,9 @@
+from .check_metaclass import check_module_properties
+
+
 class ModuleMetaclass(type):
     def __init__(cls, name, bases, clsdict) -> None:
-        if bases and len(bases) > 1:
-            raise TypeError(
-                f"You cannot declare multiple inheritance of a module class. Please check implementation of {name}"
-            )
-
-        if bases:
-            base: type = bases[0]
-            for method_name, _ in vars(base).items():
-                pass
-                # TODO: Verify propeties here
+        # TODO: Verify propeties here
+        check_module_properties(cls, name, bases)
 
         super(ModuleMetaclass, cls).__init__(name, bases, clsdict)
